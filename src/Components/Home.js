11cb0a44d4579a188ab39bc6d74a8bce17d4a810
tomas <tomas.abraham@outlook.es>
2022-12-08 04:10:07 -0300

    Capital

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="home section__padding" id="home">
-      <p className=" tracking-in-expand">Hi, i am Tomas Abraham</p>
+      <p className=" tracking-in-expand">Hi, I am Tomas Abraham</p>
       <h2 className="home-subtitle text-focus-in">Fullstack Developer</h2>
       <div className="header__button">
         <a href="#projects">
